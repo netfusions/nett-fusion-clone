4d4cb627daa5a81396a4f9f2bbac69bb267d7b96
netfusions <nettfusions@gmail.com>
2025-10-29 16:47:33 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import { HashRouter } from "react-router-dom";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <HashRouter>
-    <App />
-  </HashRouter>
-);
+createRoot(document.getElementById("root")!).render(<App />);
